8b6f95f0ae51f39b917a16e964a0ca5c5583027b
Muhammed Kaplan <gokdeniz91@gmail.com>
2018-09-10 20:27:52 +0200

    make secret key compatible with travis

--- src/root/settings.py
+++ src/root/settings.py
@@ -23,7 +23,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY') or os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
