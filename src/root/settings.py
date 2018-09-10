6fc835e26c59fe1251630d8afb94529ca0b6f999
Muhammed Kaplan <gokdeniz91@gmail.com>
2018-09-10 20:41:07 +0200

    turn on debug as default

--- src/root/settings.py
+++ src/root/settings.py
@@ -26,9 +26,9 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY') or os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=False)
+DEBUG = env('DEBUG', default=True)
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', default=[])
 
  # Application definition
 
